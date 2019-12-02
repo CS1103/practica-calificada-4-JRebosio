c415acbe893169ae5d6ecc63f3bc9052d3cf0842
[JRebosio] <[jorge.rebosio@utec.edu.pe]>
2019-12-02 16:41:34 -0500

    add test

--- main.cpp
+++ main.cpp
@@ -16,14 +16,20 @@ using namespace std;
 
 int main()
 {
-    unsigned int w,h;
+    unsigned int w,h,cw,ch;
     auto image = decode("../in.png", w, h);
-    vector<unsigned char> image2,image3;
+    vector<unsigned char> image2,image3,dados2, dados3;
+    auto dados = decode("../dados.png", cw, ch);
+    copy(dados.begin(), dados.end(), back_inserter(dados2));
+    copy(dados.begin(), dados.end(), back_inserter(dados3));
     copy(image.begin(), image.end(), back_inserter(image2));
     copy(image.begin(), image.end(), back_inserter(image3));
 
     // Un comment if you want to check buffer content
     //encode("../out.png", image, w, h);
+    Green("../dadosgreen.jpg",dados2,cw,ch);
+    Red("../dadosred.jpg",dados3,cw,ch);
+    Blue("../dadosblue.jpg",dados,cw,ch);
     Blue("../b.png",image,w,h);
     Green("../g.png",image2,w,h);
     Red("../r.png",image3,w,h);
